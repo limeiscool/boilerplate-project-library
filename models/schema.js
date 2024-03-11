fed7fb626d626d859e37061a6d5da690a72150ed
Richard Marshall <rich.qazwsx@gmail.com>
2024-03-11 03:06:38 -0700

    inprogress: post testing

--- models/schema.js
+++ models/schema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
+const commentsSchema = new mongoose.Schema({
+  comment: { type: String },
+  date: { type: Date, default: Date.now },
+});
+
 const BookSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  comments: [
-    {
-      comment: { type: String, required: true },
-      date: { type: Date, default: Date.now },
-    },
-  ],
+  comments: [commentsSchema],
 });
 
 const Book = mongoose.model("Book", BookSchema, "books");
--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,59 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
+const Book = require("../models/schema.js");
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(function (req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(async function (req, res) {
       let title = req.body.title;
+      if (!title || title.length === 0)
+        return res.status(400).json({ error: "missing required field title" });
+
+      let newBook = new Book({ title: title });
+      await newBook.save();
+      res.json({ _id: newBook._id.toString(), title: newBook.title });
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
+      Book.findByIdAndDelete(bookid, (err, docs) => {
+        if (err) {
+          return res.status(500).json("no book exists");
+        } else {
+          return res.status(200).json({ success: "delete successful" });
+        }
+      });
       //if successful response will be 'delete successful'
     });
-  
 };
